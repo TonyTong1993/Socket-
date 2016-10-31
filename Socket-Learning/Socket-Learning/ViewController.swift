88c7fe6ac81b81ba6df5a04b2d416c7c25e5de25
童万华 <tongwanhua@tongwanhuademac.local>
2016-10-31 08:07:05 -0700

    init project

---
+++ Socket-Learning/Socket-Learning/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Socket-Learning
+//
+//  Created by 童万华 on 16/10/31.
+//  Copyright © 2016年 童万华. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
